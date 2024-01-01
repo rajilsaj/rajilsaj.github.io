4352d69190909c097718c1ef554f4a4f59f7bd35
rajilsaj <svembe@gmail.com>
2024-01-01 08:55:58 -0500

    WIP keep the sidebar open

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-(function(document) {
+/*(function(document) {
   var toggle = document.querySelector('.sidebar-toggle');
   var sidebar = document.querySelector('#sidebar');
   var checkbox = document.querySelector('#sidebar-checkbox');
@@ -11,5 +11,23 @@
        (target === checkbox || target === toggle)) return;
 
     checkbox.checked = false;
+  }, false);
+})(document);
+*/
+(function(document) {
+  var toggle = document.querySelector('.sidebar-toggle');
+  var sidebar = document.querySelector('#sidebar');
+  var checkbox = document.querySelector('#sidebar-checkbox');
+
+  // Set the checkbox state to true (checked) by default
+  checkbox.checked = true;
+
+  document.addEventListener('click', function(e) {
+    var target = e.target;
+
+    // Keep the sidebar open all the time
+    if (sidebar.contains(target) || (target === checkbox || target === toggle)) return;
+
+    checkbox.checked = true; // Ensure the checkbox stays checked
   }, false);
 })(document);
